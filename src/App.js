6e8f14b495486550f894a3585871dbbdd9f175d6
Pavana004 <101943293+Pavana004@users.noreply.github.com>
2022-06-19 10:32:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Table from './table';
+import Teacher from './teacher';
+
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='row'>
+        <Table/>
+        <Teacher/>
+      </div>
+
+
+    </div>
+
+  );
+}
+
+export default App;
